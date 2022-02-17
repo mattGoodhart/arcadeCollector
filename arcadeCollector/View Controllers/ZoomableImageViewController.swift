873f4557657b08a2353c7602b68f38df02614603
Matt Goodhart <mgoodhart@google.com>
2022-02-17 11:16:59 -0500

    App Icons added to assets

--- arcadeCollector/View Controllers/ZoomableImageViewController.swift
+++ arcadeCollector/View Controllers/ZoomableImageViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ZoomableImageViewController: UIViewController, UIScrollViewDelegate {
     
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var scrollView: UIScrollView! // at this point do I even need it in Storyboard?
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     var image: UIImage?
@@ -23,99 +23,84 @@ class ZoomableImageViewController: UIViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        //  appDelegate.allowedOrientations = .all
+        initializeView()
+        setConstraints()
+    }
+    
+    func initializeView() {
         scrollView.delegate = self
         scrollView.minimumZoomScale = 1.0
         scrollView.maximumZoomScale = 5.0
         scrollView.zoomScale = 1.0
-        
-      //  scrollView.translatesAutoresizingMaskIntoConstraints = false
-      //  scrollView.frame = view.frame
-     
-        self.imageView = UIImageView(image: image)
-        
-        contentView = UIView(frame: scrollView.frame)
-        //   let contentView = UIView(frame: scrollView.frame)
-        contentView.backgroundColor = backgroundColor
+        imageView = UIImageView(image: image)
+        contentView = UIView(frame: imageView.frame)
+        contentView.backgroundColor = UIColor.blue
         view.backgroundColor = backgroundColor
         contentView.addSubview(imageView)
         scrollView.addSubview(contentView)
-        
-        setView()
-        
-        
     }
     
+    func setConstraints() {
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        contentView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
     
-    
-    
-    func setView() {
         if !isInGameImage {
             self.imageView.frame = view.frame
             self.imageView.contentMode = .scaleAspectFit
+   
         } else {
             forceAspectForInGameImage()
         }
     }
     
-    
     func forceAspectForInGameImage() {
         
-    //    contentView.frame = view.frame
-        
         let margins: UILayoutGuide = view.layoutMarginsGuide
-        let portraitHeightAnchor: NSLayoutConstraint
-        let portraitWidthAnchor: NSLayoutConstraint
-        let centerX: NSLayoutConstraint
-        let centerY: NSLayoutConstraint
+        let yokoHeightAnchor: NSLayoutConstraint
+        let tateHeightAnchor: NSLayoutConstraint
         
         imageView.contentMode = .scaleToFill
-        
-     //   contentView.translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
         
+        imageView.centerXAnchor.constraint(equalTo: margins.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: margins.centerYAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalTo: margins.widthAnchor).isActive = true
         
-//          scrollView.translatesAutoresizingMaskIntoConstraints = false
-//          scrollView.frame = view.frame
-       
-      //  contentView.frame = view.frame
-        
-        centerX = imageView.centerXAnchor.constraint(equalTo: margins.centerXAnchor)
-        centerY = imageView.centerYAnchor.constraint(equalTo: margins.centerYAnchor)
-        portraitHeightAnchor = imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 3.0/4.0)
-        portraitWidthAnchor = imageView.widthAnchor.constraint(equalTo: margins.widthAnchor)
-        
-        centerX.isActive = true
-        centerY.isActive = true
-        
+        yokoHeightAnchor = imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 3.0/4.0)
+        tateHeightAnchor = imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 4.0/3.0)
         
         switch orientation {
             
         case "Horizontal":
-            
-            portraitWidthAnchor.isActive = true
-            portraitHeightAnchor.isActive = true
+            yokoHeightAnchor.isActive = true
             
         case "Vertical":
-            
-            imageView.widthAnchor.constraint(equalTo: margins.widthAnchor).isActive = true
-            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 4.0/3.0).isActive = true
-            
+            tateHeightAnchor.isActive = true
             
         default: return
         }
+        
     }
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        
-       // return imageView
-        if isInGameImage {
-            return contentView
-        } else {
-            return imageView
-        }
-        
+//
+//        if isInGameImage {
+//            return contentView
+//        } else {
+//            return imageView
+//        }
+        return imageView
     }
 }
