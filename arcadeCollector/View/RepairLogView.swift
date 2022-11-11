6e084bccb3752784d969fbb93751aa3aea3f8820
Matt Goodhart <matt.goodhart@willowtreeapps.com>
2022-11-11 11:54:29 -0500

    All the stuff I forgot in first commit on this branch

--- arcadeCollector/View/RepairLogView.swift
+++ arcadeCollector/View/RepairLogView.swift
@@ -14,14 +14,4 @@ class RepairLogView: UIView {
     @IBOutlet weak var logNotes: UITextView!
     @IBOutlet weak var addPhotoButton: UIButton!
     
-    //logNotes.sizeToFit()
-//    init(date: UILabel!, logNotes: UITextView!, addPhotoButton: UIButton!) {
-//        self.date = date
-//        self.logNotes = logNotes
-//        self.addPhotoButton = addPhotoButton
-//    }
-//    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
 }
